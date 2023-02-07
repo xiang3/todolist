86ed73bfba7f0492d35cc7f5244a972d2a42a977
xiang3 <44763411+xiang3@users.noreply.github.com>
2023-02-07 23:07:48 +0800

    update TodoRepository and related test

--- src/test/kotlin/com/example/todolist/repository/TodoRepositoryImplTest.kt
+++ src/test/kotlin/com/example/todolist/repository/TodoRepositoryImplTest.kt
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.util.*
 
 @ExtendWith(SpringExtension::class)
 @DataJpaTest(excludeAutoConfiguration = [TodoRepositoryImpl::class])
+@Import(value = [TodoRepositoryImpl::class])
 internal class TodoRepositoryImplTest {
     @Autowired
     private lateinit var jpaTodoRepository: JpaTodoRepository
